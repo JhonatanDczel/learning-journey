0f8a1374d6066b64344cf281743acb9e551c1c2a
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-19 01:07:57 -0500

    Estylos en linea react

---
+++ react/react-course/src/Task.js
@@ -0,0 +1,8 @@
+export function TaskCard(){
+  // ejemplo con estilos en linea
+  const taskStyles = {background: "#202020", color: "white", padding: "20px"}
+  return <div style={taskStyles}>
+    <h1>Mi primera tarea</h1>
+    <p>Tarea realizada</p>
+  </div>
+}
--- react/react-course/src/index.js
+++ react/react-course/src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Saludo, UserCard} from "./Saludo"
-import Product, {Navbar} from './Product'
+import { Saludo, UserCard } from "./Saludo";
+import Product, { Navbar } from "./Product";
 import Component from "./Component";
+import { TaskCard } from "./Task";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-
-
 
 root.render(
   // Etiqueta especial fragment <></>: etiueta que contiene otras etiquetas sin ser una etiqueta en si misma
   // Proptypes para restringir el tipado
   <>
-  <Component order="1"/>
-  <Component order="2"/>
-  <Component order="3"/>
+    <TaskCard />
   </>
 );
