a49e1b117ca35e988399928feee5fde83c5099c2
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-19 02:04:33 -0500

    Jugando con bibliotecas de terceros react-icons

--- react/react-course/src/Posts.js
+++ react/react-course/src/Posts.js
@@ -1,10 +1,15 @@
+import { VscBug } from "react-icons/vsc";
+// Esto se puede hacer para usar componentes hechos por otras personas y no tenr que escirbirlo manualmente nosotros, esto es muy gooooood
 export const Post = () => {
-  async function data(){
-    let fetchData = await fetch("https://jsonplaceholder.typicode.com/posts")
-    let jsonData = await fetchData.json()
-    console.log(jsonData)
+  async function data() {
+    let fetchData = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let jsonData = await fetchData.json();
+    console.log(jsonData);
   }
   return (
-    <button onClick={data}>Traer datos</button>
-  )
-}
+    <button onClick={data}>
+      <VscBug />
+      Traer datos
+    </button>
+  );
+};
