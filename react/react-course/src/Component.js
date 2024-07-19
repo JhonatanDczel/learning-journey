02a39c2fa7ef462f59c6ac78d1ee761426b5a364
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-19 00:59:02 -0500

    PropType in javascript

---
+++ react/react-course/src/Component.js
@@ -0,0 +1,3 @@
+export default function Component(props){
+  return <h1>Componente: {props.order}</h1>
+}
--- react/react-course/src/index.js
+++ react/react-course/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Saludo, UserCard} from "./Saludo"
 import Product, {Navbar} from './Product'
+import Component from "./Component";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
@@ -10,9 +11,10 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   // Etiqueta especial fragment <></>: etiueta que contiene otras etiquetas sin ser una etiqueta en si misma
+  // Proptypes para restringir el tipado
   <>
-    <Saludo/>
-    <UserCard/>
-    <Product/>
+  <Component order="1"/>
+  <Component order="2"/>
+  <Component order="3"/>
   </>
 );
