b73f69ed346d8c4d4008cd4a13d6a6db7124f32e
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-19 00:14:37 -0500

    Trabajando con JSX

--- react/react-course/src/index.js
+++ react/react-course/src/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById('root')
-const root = ReactDOM.createRoot(rootElement)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
-function Saludo(){
-  let name = "Jhonatan"
-  return <h1>Hello world, I am { name }</h1>
+function Saludo() {
+  // let name = "Jhonatan";
+  let married = true;
+  // return <h1>Hello world, I am { name }</h1>
+  // Operadores ternarios:
+  // return married ? <h1>Casado</h1> : <h1>No casado</h1>;
+
+  const user = {
+    fn : "Jhonatan", 
+    ln : "Arias"
+  }
+
+  return <h1>{JSON.stringify(user)}</h1>
 }
 
-root.render(<Saludo/>)
+root.render(
+  <>
+    <Saludo>
+      <Saludo />
+    </Saludo>
+  </>
+);
