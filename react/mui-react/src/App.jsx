b284c57361f78dec533daee223bdc148c58d3454
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-20 22:38:06 -0500

    Agregando reset de estilos

--- react/mui-react/src/App.jsx
+++ react/mui-react/src/App.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 
 export default function App() {
   return (
-    <>
+    <Container>
       <h1>App</h1>
       <Button variant="contained"> Mi primer boton </Button>
-    </>
+    </Container>
   );
 }
