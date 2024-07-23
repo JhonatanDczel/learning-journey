489aa131da9c12098c6c3d07875734fef30f1c96
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-22 20:25:16 -0500

    Django review"

--- react/top/my-first-react-app/src/Tasking.jsx
+++ react/top/my-first-react-app/src/Tasking.jsx
@@ -1,23 +1,38 @@
-import { v4 as uuid} from 'uuid'
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 const todos = [
-  { task: "Comer", id: uuid()},
-  { task: "Dormri", id: uuid()},
-  { task: "Salir a correr", id: uuid()},
-  { task: "Estudiar", id: uuid()},
-  { task: "Comer  mientras estudias", id: uuid()},
-]
+  { task: "Comer", id: uuid() },
+  { task: "Dormri", id: uuid() },
+  { task: "Salir a correr", id: uuid() },
+  { task: "Estudiar", id: uuid() },
+  { task: "Comer  mientras estudias", id: uuid() },
+];
 
-function TaskList(){
+function TaskList() {
+  const [first, setfirst] = useState(false)
+
+  setTimeout(() => {
+    setfirst(true)
+  }, 3000)
+
+
+  setTimeout(() => {
+    setfirst(false)
+  }, 5000)
+
+  if(first)
+    return <h1>simon</h1>
+  else
+    return <h1>Nel</h1>
+
   return (
     <ul>
-      {
-        todos.map( todo => {
-          return <li key={todo.id}>{todo.task}</li>
-        })
-      }
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.task}</li>;
+      })}
     </ul>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
--- react/top/my-first-react-app/src/main.jsx
+++ react/top/my-first-react-app/src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Saludados from './Greeting'
-import {App} from "./Greeting"
 import TaskList from './Tasking'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Saludados />
     <TaskList />
   </React.StrictMode>,
 )
